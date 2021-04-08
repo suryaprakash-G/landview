05da5f49211122e30bb063e68bdc97e677b291a9
surya <suryagopinathan@gmail.com>
2021-04-08 23:58:35 +0530

    upload method found directory pending

--- server/upload.php
+++ server/upload.php
@@ -1,1 +1,15 @@
-<?php?>
+<?php
+    $image_base64 =  file_get_contents("php://input");
+    // path to admin/
+$this_dir = dirname(__FILE__);
+
+// admin's parent dir path can be represented by admin/..
+$parent_dir = realpath($this_dir . '/..');
+
+// concatenate the target path from the parent dir path
+$target_path = $parent_dir . '/landview/images/' . "new" . '.png';
+echo $target_path;
+    $myfile = fopen($target_path, "w") or die("Unable to open file!");
+    fwrite($myfile,$image_base64);
+    fclose($myfile);
+?>
